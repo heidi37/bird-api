de22e1d629dc0afc0881c298f9060933a9f36830
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-11-24 21:11:49 -0500

    updating port variable for render

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.get('/api/birds/:name',(request, response) => {
   }
 })
 
-const PORT = 3001
+const port = process.env.PORT || 3001;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
 })
