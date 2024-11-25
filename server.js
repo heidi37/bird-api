4be5e3ecce650b3bd928ed660dd66d4cbaca5ea7
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-11-25 06:04:31 -0500

    fixed cors errors

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 let data = require('./data')
+
+app.use(cors())
 
 app.get('/', (request, response) => {
   response.send(data)
