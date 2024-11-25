6b0ac3d8bc0cadea1838ed6e9a7bddd8f951f223
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-11-25 06:33:21 -0500

    add full url to image field

--- data.js
+++ data.js
@@ -3,25 +3,25 @@ const birdData = {
     latinName: "Turdus migratorius",
     order: "Passeriformes",
     family: "Turdidae(Thrushes)",
-    image: "/images/american-robin.jpg"
+    image: "https://bird-api-hdc8.onrender.com/images/american-robin.jpg"
   },
     "American Goldfinch": {
     latinName: "Spinus tristis",
     order: "Passeriformes",
     family: "Fringillidae (Finches)",
-    image: "/images/american-goldfinch.jpg"
+    image: "https://bird-api-hdc8.onrender.com/images/american-goldfinch.jpg"
   },
     "Eastern Phoebe": {
     latinName: "Sayornis phoebe",
     order: "Passeriformes",
     family: "Tyrannidae (Flycatchers)",
-    image: "/images/eastern-phoebe.jpg"
+    image: "https://bird-api-hdc8.onrender.com/images/eastern-phoebe.jpg"
     },
     "unknown": {
     latinName: "unknown",
     order: "unknown",
     family: "unknown",
-    image: "unknown"
+    image: "unknowng"
     }
   }
 
