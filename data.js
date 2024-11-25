4fe1df704b97c2d6f41ca0622b6bbfafa12a5b4b
Heidi Fryzell <heidi37@users.noreply.github.com>
2024-11-24 21:00:58 -0500

    API functional

---
+++ data.js
@@ -0,0 +1,28 @@
+const birdData = {
+    "American Robin": {
+    latinName: "Turdus migratorius",
+    order: "Passeriformes",
+    family: "Turdidae(Thrushes)",
+    image: "/images/american-robin.jpg"
+  },
+    "American Goldfinch": {
+    latinName: "Spinus tristis",
+    order: "Passeriformes",
+    family: "Fringillidae (Finches)",
+    image: "/images/american-goldfinch.jpg"
+  },
+    "Eastern Phoebe": {
+    latinName: "Sayornis phoebe",
+    order: "Passeriformes",
+    family: "Tyrannidae (Flycatchers)",
+    image: "/images/eastern-phoebe.jpg"
+    },
+    "unknown": {
+    latinName: "unknown",
+    order: "unknown",
+    family: "unknown",
+    image: "unknown"
+    }
+  }
+
+module.exports = birdData;
---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+let data = require('./data')
+
+app.get('/', (request, response) => {
+  response.send(data)
+})
+
+app.get('/api/birds/:name',(request, response) => {
+  const birdName = request.params.name
+  if(data[birdName]){
+      response.json(data[birdName])
+  }else{
+      response.json(data['unknown'])
+  }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`)
+})
